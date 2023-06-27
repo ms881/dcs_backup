2b53ef1e7a88dd27e2dcfca57092523c7e6b56c2
ms881 <137846353+ms881@users.noreply.github.com>
2023-06-26 21:31:42 -0400

    Add files via upload

---
+++ DCS.openbeta/Liveries/FA-18C_hornet/VFA-51 Line Alternate/description.lua
@@ -0,0 +1,207 @@
+livery = {
+	--Main Maps
+	{"f18c1", 0 ,"F18C_1_DIFF_VF51_Line_alt",false};
+	{"f18c1", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+
+	{"f18c2", 0 ,"F18C_2_DIFF_VF51_Line_alt",false};
+	{"f18c2", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	
+	--Fuel Tanks
+	{"FPU_8A", 0 ,"FPU_8A_VF51_Fuel",false};
+	{"FPU_8A", ROUGHNESS_METALLIC ,"FPU_8A_Diff_RoughMet",true};
+	
+	--Nose Aircraft Numbers (USN)
+	--Left
+	{"F18C_BORT_NUMBER_NOSE_L_100", 0 ,"F18C_1_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_NOSE_L_100", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_NOSE_L_100", DECAL ,"F18C_BORT_NUM_ITAL_RIGHT_LINE_A",false};
+	{"F18C_BORT_NUMBER_NOSE_L_10", 0 ,"F18C_1_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_NOSE_L_10", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_NOSE_L_10", DECAL ,"F18C_BORT_NUM_ITAL_RIGHT_LINE_A",false};	
+	{"F18C_BORT_NUMBER_NOSE_L_01", 0 ,"F18C_1_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_NOSE_L_01", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_NOSE_L_01", DECAL ,"F18C_BORT_NUM_ITAL_RIGHT_LINE_A",false};
+	--Right
+	{"F18C_BORT_NUMBER_NOSE_R_100", 0 ,"F18C_1_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_NOSE_R_100", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_NOSE_R_100", DECAL ,"F18C_BORT_NUM_ITAL_LEFT_LINE_A",false};
+	{"F18C_BORT_NUMBER_NOSE_R_10", 0 ,"F18C_1_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_NOSE_R_10", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_NOSE_R_10", DECAL ,"F18C_BORT_NUM_ITAL_LEFT_LINE_A",false};	
+	{"F18C_BORT_NUMBER_NOSE_R_01", 0 ,"F18C_1_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_NOSE_R_01", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_NOSE_R_01", DECAL ,"F18C_BORT_NUM_ITAL_LEFT_LINE_A",false};	
+		
+	--Flap Aircraft Numbers (USN)
+	--Left
+	{"F18C_BORT_NUMBER_ZAK_L_100", 0 ,"F18C_2_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_ZAK_L_100", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_ZAK_L_100", DECAL ,"F18C_BORT_NUM_NO_ITAL_LINE_A",false};
+	{"F18C_BORT_NUMBER_ZAK_L_10", 0 ,"F18C_2_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_ZAK_L_10", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_ZAK_L_10", DECAL ,"F18C_BORT_NUM_NO_ITAL_LINE_A",false};	
+	{"F18C_BORT_NUMBER_ZAK_L_01", 0 ,"F18C_2_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_ZAK_L_01", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_ZAK_L_01", DECAL ,"F18C_BORT_NUM_NO_ITAL_LINE_A",false};
+	--Right
+	{"F18C_BORT_NUMBER_ZAK_R_100", 0 ,"F18C_2_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_ZAK_R_100", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_ZAK_R_100", DECAL ,"F18C_BORT_NUM_NO_ITAL_LINE_A",false};
+	{"F18C_BORT_NUMBER_ZAK_R_10", 0 ,"F18C_2_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_ZAK_R_10", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_ZAK_R_10", DECAL ,"F18C_BORT_NUM_NO_ITAL_LINE_A",false};	
+	{"F18C_BORT_NUMBER_ZAK_R_01", 0 ,"F18C_2_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_ZAK_R_01", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_ZAK_R_01", DECAL ,"F18C_BORT_NUM_NO_ITAL_LINE_A",false};
+	
+	--Vertical Stab Aircraft Numbers (USN)
+	--Left
+	{"F18C_BORT_NUMBER_KIL_L_100", 0 ,"F18C_2_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_KIL_L_100", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_L_100", DECAL ,"F18C_BORT_NUM_ITAL_RIGHT_LINE_A",false};
+	{"F18C_BORT_NUMBER_KIL_L_10", 0 ,"F18C_2_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_KIL_L_10", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_L_10", DECAL ,"F18C_BORT_NUM_ITAL_RIGHT_LINE_A",false};	
+	{"F18C_BORT_NUMBER_KIL_L_01", 0 ,"F18C_2_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_KIL_L_01", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_L_01", DECAL ,"F18C_BORT_NUM_ITAL_RIGHT_LINE_A",false};	
+	--Right
+	{"F18C_BORT_NUMBER_KIL_R_100", 0 ,"F18C_2_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_KIL_R_100", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_R_100", DECAL ,"F18C_BORT_NUM_ITAL_LEFT_LINE_A",false};
+	{"F18C_BORT_NUMBER_KIL_R_10", 0 ,"F18C_2_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_KIL_R_10", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_R_10", DECAL ,"F18C_BORT_NUM_ITAL_LEFT_LINE_A",false};	
+	{"F18C_BORT_NUMBER_KIL_R_01", 0 ,"F18C_2_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_KIL_R_01", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_R_01", DECAL ,"F18C_BORT_NUM_ITAL_LEFT_LINE_A",false};
+
+	--Nose Aircraft Numbers (KUWAIT)
+	--Left
+	{"F18C_BORT_NUMBER_NOSE_kuw_L_100", 0 ,"F18C_1_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_NOSE_kuw_L_100", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_NOSE_kuw_L_100", DECAL ,"empty",true};
+	{"F18C_BORT_NUMBER_NOSE_kuw_L_10", 0 ,"F18C_1_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_NOSE_kuw_L_10", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_NOSE_kuw_L_10", DECAL ,"empty",true};	
+	{"F18C_BORT_NUMBER_NOSE_kuw_L_01", 0 ,"F18C_1_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_NOSE_kuw_L_01", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_NOSE_kuw_L_01", DECAL ,"empty",true};
+	--Right
+	{"F18C_BORT_NUMBER_NOSE_kuw_R_100", 0 ,"F18C_1_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_NOSE_kuw_R_100", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_NOSE_kuw_R_100", DECAL ,"empty",true};
+	{"F18C_BORT_NUMBER_NOSE_kuw_R_10", 0 ,"F18C_1_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_NOSE_kuw_R_10", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_NOSE_kuw_R_10", DECAL ,"empty",true};	
+	{"F18C_BORT_NUMBER_NOSE_kuw_R_01", 0 ,"F18C_1_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_NOSE_kuw_R_01", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_NOSE_kuw_R_01", DECAL ,"empty",true};		
+	
+
+	--Vertical Stab Numbers (KUWAIT)
+	--Right
+	{"F18C_BORT_NUMBER_KIL_Kuw_R_100", 0 ,"F18C_2_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_KIL_Kuw_R_100", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_Kuw_R_100", DECAL ,"empty",true};
+	{"F18C_BORT_NUMBER_KIL_Kuw_R_10", 0 ,"F18C_2_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_KIL_Kuw_R_10", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_Kuw_R_10", DECAL ,"empty",true};	
+	{"F18C_BORT_NUMBER_KIL_Kuw_R_01", 0 ,"F18C_2_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_KIL_Kuw_R_01", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_Kuw_R_01", DECAL ,"empty",true};	
+	--Left
+	{"F18C_BORT_NUMBER_KIL_Kuw_L_100", 0 ,"F18C_2_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_KIL_Kuw_L_100", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_Kuw_L_100", DECAL ,"empty",true};
+	{"F18C_BORT_NUMBER_KIL_Kuw_L_10", 0 ,"F18C_2_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_KIL_Kuw_L_10", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_Kuw_L_10", DECAL ,"empty",true};	
+	{"F18C_BORT_NUMBER_KIL_Kuw_L_01", 0 ,"F18C_2_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_KIL_Kuw_L_01", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_Kuw_L_01", DECAL ,"empty",true};		
+
+	--Nose Aircraft Numbers (FINLAND)
+	--Left
+	{"F18C_BORT_NUMBER_NOSE_fin_L_10", 0 ,"F18C_1_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_NOSE_fin_L_10", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_NOSE_fin_L_10", DECAL ,"empty",true};	
+	{"F18C_BORT_NUMBER_NOSE_fin_L_01", 0 ,"F18C_1_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_NOSE_fin_L_01", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_NOSE_fin_L_01", DECAL ,"empty",true};
+	--Right
+	{"F18C_BORT_NUMBER_NOSE_fin_R_10", 0 ,"F18C_1_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_NOSE_fin_R_10", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_NOSE_fin_R_10", DECAL ,"empty",true};	
+	{"F18C_BORT_NUMBER_NOSE_fin_R_01", 0 ,"F18C_1_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_NOSE_fin_R_01", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_NOSE_fin_R_01", DECAL ,"empty",true};			
+
+	--Vertical Tail Numbers (SWITZERLAND)
+	--Right
+	{"F18C_BORT_NUMBER_KIL_Switz_R_100", 0 ,"F18C_2_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_KIL_Switz_R_100", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_Switz_R_100", DECAL ,"empty",true};
+	{"F18C_BORT_NUMBER_KIL_Switz_R_10", 0 ,"F18C_2_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_KIL_Switz_R_10", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_Switz_R_10", DECAL ,"empty",true};	
+	{"F18C_BORT_NUMBER_KIL_Switz_R_01", 0 ,"F18C_2_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_KIL_Switz_R_01", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_Switz_R_01", DECAL ,"empty",true};	
+	--Left
+	{"F18C_BORT_NUMBER_KIL_Switz_L_100", 0 ,"F18C_2_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_KIL_Switz_L_100", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_Switz_L_100", DECAL ,"empty",true};
+	{"F18C_BORT_NUMBER_KIL_Switz_L_10", 0 ,"F18C_2_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_KIL_Switz_L_10", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_Switz_L_10", DECAL ,"empty",true};	
+	{"F18C_BORT_NUMBER_KIL_Switz_L_01", 0 ,"F18C_2_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_KIL_Switz_L_01", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_Switz_L_01", DECAL ,"empty",true};
+
+	--Gear Door Numbers (RAAF)
+	--Right
+	{"F18C_BORT_NUMBER_STV_aus_R_10", 0 ,"F18C_1_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_STV_aus_R_10", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_STV_aus_R_10", DECAL ,"empty",true};	
+	{"F18C_BORT_NUMBER_STV_aus_R_01", 0 ,"F18C_1_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_STV_aus_R_01", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_STV_aus_R_01", DECAL ,"empty",true};	
+	--Left
+	{"F18C_BORT_NUMBER_STV_aus_L_10", 0 ,"F18C_1_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_STV_aus_L_10", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_STV_aus_L_10", DECAL ,"empty",true};	
+	{"F18C_BORT_NUMBER_STV_aus_L_01", 0 ,"F18C_1_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_STV_aus_L_01", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_STV_aus_L_01", DECAL ,"empty",true};		
+		
+	--Aft Fuselage Numbers (RAAF)
+	--Right
+	{"F18C_BORT_NUMBER_MTW_aus_R_10", 0 ,"F18C_1_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_MTW_aus_R_10", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_MTW_aus_R_10", DECAL ,"empty",true};	
+	{"F18C_BORT_NUMBER_MTW_aus_R_01", 0 ,"F18C_1_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_MTW_aus_R_01", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_MTW_aus_R_01", DECAL ,"empty",true};	
+	--Left
+	{"F18C_BORT_NUMBER_MTW_aus_L_10", 0 ,"F18C_1_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_MTW_aus_L_10", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_MTW_aus_L_10", DECAL ,"empty",true};	
+	{"F18C_BORT_NUMBER_MTW_aus_L_01", 0 ,"F18C_1_DIFF_VF51_Line_alt",false};
+	{"F18C_BORT_NUMBER_MTW_aus_L_01", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_MTW_aus_L_01", DECAL ,"empty",true};		
+}
+custom_args = 
+{
+[27]   = 0.0, -- Tail      change of type of board number (0.0 -default USA, 0.1- )
+[1000] = 0.0, -- Flaps
+[1001] = 0.0, -- Nose
+[1002] = 1, -- Kuwait Squadron 
+[1003] = 1, -- Australian Squadron 
+[1004] = 1, -- Finland Squadron
+[1005] = 1, -- Switzerland Squadron
+[1006] = 1, -- Blue Angels Jet Team
+}
+name = "VFA-51 Screaming Eagles (Line Alternate)"
+countries = {"USA",}
+--By 51st PVO X-man
